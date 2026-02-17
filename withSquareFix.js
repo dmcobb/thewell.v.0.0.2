e4d43ba54dbcc432f11193e5b0287e8179e77463
Tone King Development <tone@tonekingdev.com>
2026-02-17 16:55:04 -0500

    switched to expo/config-plugins

--- withSquareFix.js
+++ withSquareFix.js
@@ -1,4 +1,4 @@
-const { withXcodeProject } = require('@expo/config-plugins');
+const { withXcodeProject } = require('expo/config-plugins');
 
 const withSquareFix = (config) => {
   return withXcodeProject(config, (config) => {
